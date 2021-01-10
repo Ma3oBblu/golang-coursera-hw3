544a6c9314f87e4abb1d709a41168d8bda709e39
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2021-01-10 16:38:10 +0300

    microfixes
    BenchmarkSlow-8               42          29084621 ns/op        18910948 B/op     195792 allocs/op
    BenchmarkFast-8              123           9602761 ns/op         1747886 B/op      55489 allocs/op

--- fast.go
+++ fast.go
@@ -18,73 +18,29 @@ func FastSearch(out io.Writer) {
 	}
 	defer file.Close()
 
-	var seenBrowsers []string
-	var uniqueBrowsers int
+	seenBrowsers := make(map[string]struct{})
 	var foundUsers string
-	//users := make([]map[string]interface{}, 0)
 
 	scanner := bufio.NewScanner(file)
 	i := 0
 	user := make(map[string]interface{})
 	for scanner.Scan() {
-		// fmt.Printf("%v %v\n", err, line)
-		err := json.Unmarshal(scanner.Bytes(), &user)
+		i++
+		err = json.Unmarshal(scanner.Bytes(), &user)
 		if err != nil {
 			panic(err)
 		}
 
-		i++
-
-		isAndroid := false
-		isMSIE := false
-
-		browsers, ok := user["browsers"].([]interface{})
-		if !ok {
-			// log.Println("cant cast browsers")
-			continue
-		}
-
+		var isAndroid, isMSIE bool
+		browsers := user["browsers"].([]interface{})
 		for _, browserRaw := range browsers {
-			browser, ok := browserRaw.(string)
-			if !ok {
-				// log.Println("cant cast browser to string")
-				continue
-			}
+			browser := browserRaw.(string)
 			if strings.Contains(browser, "Android") {
 				isAndroid = true
-				notSeenBefore := true
-				for _, item := range seenBrowsers {
-					if item == browser {
-						notSeenBefore = false
-					}
-				}
-				if notSeenBefore {
-					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
-					seenBrowsers = append(seenBrowsers, browser)
-					uniqueBrowsers++
-				}
-			}
-		}
-
-		for _, browserRaw := range browsers {
-			browser, ok := browserRaw.(string)
-			if !ok {
-				// log.Println("cant cast browser to string")
-				continue
-			}
-			if strings.Contains(browser, "MSIE") {
+				seenBrowsers[browser] = struct{}{}
+			} else if strings.Contains(browser, "MSIE") {
 				isMSIE = true
-				notSeenBefore := true
-				for _, item := range seenBrowsers {
-					if item == browser {
-						notSeenBefore = false
-					}
-				}
-				if notSeenBefore {
-					// log.Printf("SLOW New browser: %s, first seen: %s", browser, user["name"])
-					seenBrowsers = append(seenBrowsers, browser)
-					uniqueBrowsers++
-				}
+				seenBrowsers[browser] = struct{}{}
 			}
 		}
 
@@ -96,7 +52,7 @@ func FastSearch(out io.Writer) {
 		email := strings.ReplaceAll(user["email"].(string), "@", " [at] ")
 		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i-1, user["name"], email)
 	}
-	if err := scanner.Err(); err != nil {
+	if err = scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
